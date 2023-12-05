d7635a560c0431cce59d14342c8cabf20f42d9be
Bernardo Mattos <bressan.mattos@estudante.ufjf.br>
2023-12-05 00:31:22 -0300

    acha elementos importantes em main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+import { adicionaNaLista, getLista, limpaLista, removeDaLista } from "./lista.js";
+
+const pEntrada = document.querySelector('#entrada');
+const olSaida = document.querySelector('#itens');
+const btnAdicionar = document.querySelector('#adicionar');
+const btnLimpar = document.querySelector('#limpar');
+const btnRemover = document.querySelector('#remover');
