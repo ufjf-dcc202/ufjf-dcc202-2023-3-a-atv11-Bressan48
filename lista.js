53ea073d932067f2e850f40e8b6368163ceca787
Bernardo Mattos <bressan.mattos@estudante.ufjf.br>
2023-12-05 00:27:50 -0300

    completa lista.js

--- lista.js
+++ lista.js
@@ -2,20 +2,20 @@ const lista = ["Um", "Dois", "Três"];
 
 export function getLista()
 {
-    
+    return lista;
 }
 
 export function limpaLista()
 {
-    
+    lista.splice(0);
 }
 
 export function adicionaNaLista()
 {
-
+    lista.push(novoItem);
 }
 
 export function removeDaLista()
 {
-
+    lista.splice(posicao, 1);
 }
